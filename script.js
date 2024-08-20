5cda85e13cff966b7aeaf573a886eb036452315b
NeyleCM <neylecmorantes@gmail.com>
2024-08-20 23:25:28 +0200

    complete

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-const contentPrincipal = document.querySelector('.contentPrincipal')
+//const contentPrincipal = document.querySelector('.contentPrincipal')
 const left = document.querySelector('.left')
 const right = document.querySelector('.right')
 
-const cursorMove = () => {
-    contentPrincipal.classList.toggle('hover-change')
-}
 
-left.addEventListener('mousemove', () => 
-cursorMove('left')
-)
+left.addEventListener('mouseenter', () => {
+    left.classList.add('hover-change');
+    right.classList.remove('hover-change'); // Asegúrate de que el otro lado no esté expandido
+});
 
-right.addEventListener('mousemove', () => 
-    cursorMove('right')
-)
+right.addEventListener('mouseenter', () => {
+    right.classList.add('hover-change');
+    left.classList.remove('hover-change'); // Asegúrate de que el otro lado no esté expandido
+});
+
+left.addEventListener('mouseleave', () => {
+    left.classList.remove('hover-change');
+});
+
+right.addEventListener('mouseleave', () => {
+    right.classList.remove('hover-change');
+});
